d2814a1c67b9fee35b8d34dff5441cf1d8ae110d
Naufal As <naufal202@gmail.com>
2023-08-08 15:34:02 +0530

    added ValueListenableBuilder for BottomNavigaton bar

--- lib/home/Homepage.dart
+++ lib/home/Homepage.dart
@@ -1,41 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:pmm/screen/categorypage.dart';
 import 'package:pmm/screen/homepage.dart';
+import 'package:pmm/widget/bottomnavigaton.dart';
 
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   const HomePage({super.key});
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  int selectedindex = 0;
+  static ValueNotifier<int> selectedindex = ValueNotifier(0);
 
   @override
   Widget build(BuildContext context) {
+    List page = [HomePageScreen(), CategoryPageScreen()];
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("MONEY MANAGER"),
-      ),
-      floatingActionButton:
-          const CircleAvatar(radius: 30, child: Icon(Icons.add)),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: selectedindex,
-        onTap: (value) {
-          setState(() {
-            selectedindex = value;
-          });
-        },
-        items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.expand_circle_down), label: 'Category')
-        ],
-      ),
-      body: selectedindex == 0
-          ? const HomePageScreen()
-          : const CategoryPageScreen(),
-    );
+        appBar: AppBar(
+          title: const Text("MONEY MANAGER"),
+        ),
+        floatingActionButton:
+            const CircleAvatar(radius: 30, child: Icon(Icons.add)),
+        bottomNavigationBar: BottomNavigationWidget(),
+        body: ValueListenableBuilder(
+            valueListenable: selectedindex,
+            builder: (context, index, child) {
+              return page[index];
+            }));
   }
 }
--- lib/widget/bottomnavigaton.dart
+++ lib/widget/bottomnavigaton.dart
@@ -1,1 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:pmm/home/Homepage.dart';
 
+class BottomNavigationWidget extends StatelessWidget {
+  const BottomNavigationWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ValueListenableBuilder(
+        valueListenable: HomePage.selectedindex,
+        builder: (context, index, _) {
+          return BottomNavigationBar(
+            currentIndex: index,
+            onTap: (value) {
+              HomePage.selectedindex.value = value;
+            },
+            items: const [
+              BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.expand_circle_down), label: 'Category')
+            ],
+          );
+        });
+  }
+}
